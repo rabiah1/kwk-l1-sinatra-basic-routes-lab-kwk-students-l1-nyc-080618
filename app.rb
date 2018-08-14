b60c21c03be49719e3e4bd81396da07e24497677
rabiah1 <rabiahn01@gmail.com>
2018-08-14 15:26:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Welcome to my page!'
+  end 
+  get '/name' do 
+    'Hi, my name is Rabiah Naeem.'
+  end
+  get '/hometown' do
+    'I am from New York, Queens.'
+  end 
+  get '/favorite-song' do
+    'My favorite song is called Egotistic.'
+  end 
 end
